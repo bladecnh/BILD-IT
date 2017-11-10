e5d68d37165e24e514554872656a6875da5d7769
bladecnh <nedim1993@gmail.com>
2017-11-10 21:34:18 +0100

    Piramida zavrsena

--- Zadaca_08_11/Zadatak5.java
+++ Zadaca_08_11/Zadatak5.java
@@ -8,6 +8,8 @@ public class Zadatak5 {
         System.out.print("Unesite broj redova: ");
         int br = unos.nextInt();
 
+        System.out.println("Piramida A");
+
         for (int i = 1; i <= br; i++){
             for (int j = 1; j <= i; j++){
                 System.out.print(j);
@@ -17,13 +19,35 @@ public class Zadatak5 {
         }
 
         System.out.println();
+        System.out.println("Piramida C");
         for (int i = 1; i<=br; i++){
             for (int j=1; j<=br-i; j++){
                 System.out.print(" ");}
-            for (int j=1; j<=i; j++){
+            for (int j=i; j>=1; j--){
                 System.out.print(j);
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println("Piramida B");
+        for (int i = br; i >= 1; i--){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j);
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Piramida D");
+        for (int i = 1; i<=br; i++){
+            for (int j=1; j<i; j++){
+                System.out.print(" ");}
+            for (int j=1; j<=br-i+1; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+
     }
 }
